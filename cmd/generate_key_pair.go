1d59d5bfd6bf8cfceb322539aa5ea85e7bd8e2de
Dan Lorenc <dlorenc@google.com>
2021-02-04 20:08:14 -0600

    Generate keypairs.

--- cmd/generate_key_pair.go
+++ cmd/generate_key_pair.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"context"
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"flag"
+	"fmt"
+	"io/ioutil"
 
 	"github.com/peterbourgon/ff/v3/ffcli"
+	"golang.org/x/term"
 )
 
 func GenerateKeyPair() *ffcli.Command {
@@ -24,5 +32,55 @@ func GenerateKeyPair() *ffcli.Command {
 }
 
 func generateKeyPair(ctx context.Context) error {
+	pub, priv, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	// Encrypt the private key and store it.
+	password, err := getPass()
+	if err != nil {
+		return err
+	}
+	var pb *pem.Block
+
+	pb, err = x509.EncryptPEMBlock(rand.Reader, "ENCRYPTED PRIVATE KEY", priv, password, x509.PEMCipherAES256)
+	if err != nil {
+		return err
+	}
+
+	privBytes := pem.EncodeToMemory(pb)
+	if err := ioutil.WriteFile("cosign.key", privBytes, 0600); err != nil {
+		return err
+	}
+
+	// Now do the public key
+	pubBytes := pem.EncodeToMemory(&pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: pub,
+	})
+	if err := ioutil.WriteFile("cosign.pub", pubBytes, 0600); err != nil {
+		return err
+	}
 	return nil
+}
+
+func getPass() ([]byte, error) {
+	fmt.Print("Enter password for private key: ")
+	pw1, err := term.ReadPassword(0)
+	fmt.Println()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Print("Enter again: ")
+	pw2, err := term.ReadPassword(0)
+	fmt.Println()
+	if err != nil {
+		return nil, err
+	}
+
+	if string(pw1) != string(pw2) {
+		return nil, errors.New("passwords do not match")
+	}
+	return pw1, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/dlorenc/cosign
 
 go 1.15
 
-require github.com/peterbourgon/ff/v3 v3.0.0
+require (
+	github.com/peterbourgon/ff/v3 v3.0.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf
+)
