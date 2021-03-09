2acb0f7fcaccabdcfa8f276d27c88ccd50acc11d
Priya Wadhwa <priyawadhwa@google.com>
2021-03-09 11:32:18 -0800

    Add support for creating an asymmetric key in GCP KMS
    Signed-off-by: Priya Wadhwa <priyawadhwa@google.com>

---
+++ pkg/cosign/kms/gcp.go
@@ -0,0 +1,102 @@
+/*
+Copyright The Sigstore Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kms
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	kms "cloud.google.com/go/kms/apiv1"
+	"github.com/pkg/errors"
+	kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
+)
+
+type gcpDetails struct {
+	projectID  string
+	locationID string
+	keyRing    string
+	key        string
+}
+
+// GCP KMS keyResourceID should be in the format
+// "projects/[PROJECT_ID]/locations/[LOCATION]/keyRings/[KEY_RING]/cryptoKeys/[KEY]".
+func CreateKey(ctx context.Context, keyResourceID string) error {
+	client, err := kms.NewKeyManagementClient(ctx)
+	if err != nil {
+		return errors.Wrap(err, "new key management client")
+	}
+	deets, err := parseKeyResourceID(keyResourceID)
+	if err != nil {
+		return errors.Wrap(err, "parsing key resource ID")
+	}
+	if err := createKeyRing(ctx, client, deets); err != nil {
+		return errors.Wrap(err, "creating key ring")
+	}
+	return createKey(ctx, client, deets)
+}
+
+func parseKeyResourceID(keyResourceID string) (gcpDetails, error) {
+	details := strings.Split(keyResourceID, "/")
+	if len(details) != 8 {
+		return gcpDetails{}, errors.New("kms specification should be in the format gcpkms://projects/[PROJECT_ID]/locations/[LOCATION]/keyRings/[KEY_RING]/cryptoKeys/[KEY]")
+	}
+	return gcpDetails{
+		projectID:  details[1],
+		locationID: details[3],
+		keyRing:    details[5],
+		key:        details[7],
+	}, nil
+}
+
+func createKeyRing(ctx context.Context, client *kms.KeyManagementClient, deets gcpDetails) error {
+	getKeyRingRequest := &kmspb.GetKeyRingRequest{
+		Name: deets.keyRing,
+	}
+	if result, err := client.GetKeyRing(ctx, getKeyRingRequest); err == nil {
+		fmt.Printf("Key ring %s already exists in GCP KMS, moving on to creating key.\n", result.GetName())
+		// key ring already exists, no need to create
+		return err
+	}
+	// try to create key ring
+	createKeyRingRequest := &kmspb.CreateKeyRingRequest{
+		Parent:    fmt.Sprintf("projects/%s/locations/%s", deets.projectID, deets.locationID),
+		KeyRingId: deets.keyRing,
+	}
+	result, err := client.CreateKeyRing(ctx, createKeyRingRequest)
+	fmt.Printf("Created key ring %s in GCP KMS.\n", result.GetName())
+	return err
+}
+
+func createKey(ctx context.Context, client *kms.KeyManagementClient, deets gcpDetails) error {
+	createKeyRequest := &kmspb.CreateCryptoKeyRequest{
+		Parent:      fmt.Sprintf("projects/%s/locations/%s/keyRings/%s", deets.projectID, deets.locationID, deets.keyRing),
+		CryptoKeyId: deets.key,
+		CryptoKey: &kmspb.CryptoKey{
+			Purpose: kmspb.CryptoKey_ASYMMETRIC_SIGN,
+			VersionTemplate: &kmspb.CryptoKeyVersionTemplate{
+				Algorithm: kmspb.CryptoKeyVersion_EC_SIGN_P256_SHA256,
+			},
+		},
+	}
+	result, err := client.CreateCryptoKey(ctx, createKeyRequest)
+	if err != nil {
+		return errors.Wrap(err, "creating crypto key")
+	}
+	fmt.Printf("Created key %s in GCP KMS\n", result.GetName())
+	return nil
+}
---
+++ pkg/cosign/kms/kms.go
@@ -0,0 +1,26 @@
+/*
+Copyright The Sigstore Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kms
+
+type KMS interface {
+	// CreateKey is responsible for creating an asymmetric key pair
+	// with the ECDSA algorithm on the P-256 Curve with a SHA-256 digest
+	CreateKey(uri string) error
+
+	// Encrypt is responsible for signing
+	Encrypt() error
+}
