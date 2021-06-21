6f08789415852ad5d07ff1f44cafcb46d1612822
Jake Sanders <jsand@google.com>
2021-06-21 12:17:52 -0700

    remove dependency on `skaffold`, update deps (#380)
    Signed-off-by: Jake Sanders <jsand@google.com>

---
+++ pkg/cosign/kubernetes/client.go
@@ -0,0 +1,60 @@
+// Copyright 2021 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kubernetes
+
+import (
+	"fmt"
+
+	"k8s.io/client-go/kubernetes"
+
+	// Initialize all known client auth plugins
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func defaultClientConfig() clientcmd.ClientConfig {
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	configOverrides := &clientcmd.ConfigOverrides{}
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+}
+
+func restClientConfig() (*rest.Config, error) {
+	kubeCfg := defaultClientConfig()
+
+	restConfig, err := kubeCfg.ClientConfig()
+	if clientcmd.IsEmptyConfig(err) {
+		restConfig, err := rest.InClusterConfig()
+		if err != nil {
+			return restConfig, fmt.Errorf("error creating REST client config in-cluster: %v", err)
+		}
+
+		return restConfig, nil
+	}
+	if err != nil {
+		return restConfig, fmt.Errorf("error creating REST client config: %v", err)
+	}
+
+	return restConfig, nil
+}
+
+func Client() (kubernetes.Interface, error) {
+	config, err := restClientConfig()
+	if err != nil {
+		return nil, fmt.Errorf("getting client config for Kubernetes client: %w", err)
+	}
+	return kubernetes.NewForConfig(config)
+}
