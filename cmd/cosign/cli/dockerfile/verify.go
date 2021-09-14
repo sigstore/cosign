1b1cafccd5dac3a44ff3aabee683a6488bcc2584
Jake Sanders <jsand@google.com>
2021-09-14 13:54:40 -0700

    move `verify-dockerfile` to `dockerfile verify` (#662)
    * move `verify-dockerfile` to `dockerfile verify`
    
    Signed-off-by: Jake Sanders <jsand@google.com>
    
    * fix docstring
    
    Signed-off-by: Jake Sanders <jsand@google.com>

--- cmd/cosign/cli/dockerfile/verify.go
+++ cmd/cosign/cli/dockerfile/verify.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cli
+package dockerfile
 
 import (
 	"bufio"
@@ -25,24 +25,25 @@ import (
 
 	"github.com/peterbourgon/ff/v3/ffcli"
 	"github.com/pkg/errors"
+	"github.com/sigstore/cosign/cmd/cosign/cli"
 )
 
 // VerifyCommand verifies a signature on a supplied container image
 type VerifyDockerfileCommand struct {
-	VerifyCommand
+	cli.VerifyCommand
 	BaseOnly bool
 }
 
 // Verify builds and returns an ffcli command
 func VerifyDockerfile() *ffcli.Command {
-	cmd := VerifyDockerfileCommand{VerifyCommand: VerifyCommand{}}
-	flagset := flag.NewFlagSet("cosign verify-dockerfile", flag.ExitOnError)
+	cmd := VerifyDockerfileCommand{VerifyCommand: cli.VerifyCommand{}}
+	flagset := flag.NewFlagSet("cosign dockerfile verify", flag.ExitOnError)
 	flagset.BoolVar(&cmd.BaseOnly, "base-image-only", false, "only verify the base image (the last FROM image in the Dockerfile)")
-	applyVerifyFlags(&cmd.VerifyCommand, flagset)
+	cli.ApplyVerifyFlags(&cmd.VerifyCommand, flagset)
 
 	return &ffcli.Command{
-		Name:       "verify-dockerfile",
-		ShortUsage: "cosign verify-dockerfile -key <key path>|<key url>|<kms uri> <path/to/Dockerfile>",
+		Name:       "verify",
+		ShortUsage: "cosign dockerfile verify -key <key path>|<key url>|<kms uri> <path/to/Dockerfile>",
 		ShortHelp:  "Verify a signature on the base image specified in the Dockerfile",
 		LongHelp: `Verify signature and annotations on images in a Dockerfile by checking claims
 against the transparency log.
@@ -51,34 +52,34 @@ Shell-like variables in the Dockerfile's FROM lines will be substituted with val
 
 EXAMPLES
   # verify cosign claims and signing certificates on the FROM images in the Dockerfile
-  cosign verify-dockerfile <path/to/Dockerfile>
+  cosign dockerfile verify <path/to/Dockerfile>
 
   # only verify the base image (the last FROM image)
-  cosign verify-dockerfile -base-image-only <path/to/Dockerfile>
+  cosign dockerfile verify -base-image-only <path/to/Dockerfile>
 
   # additionally verify specified annotations
-  cosign verify-dockerfile -a key1=val1 -a key2=val2 <path/to/Dockerfile>
+  cosign dockerfile verify -a key1=val1 -a key2=val2 <path/to/Dockerfile>
 
   # (experimental) additionally, verify with the transparency log
-  COSIGN_EXPERIMENTAL=1 cosign verify-dockerfile <path/to/Dockerfile>
+  COSIGN_EXPERIMENTAL=1 cosign dockerfile verify <path/to/Dockerfile>
 
   # verify images with public key
-  cosign verify-dockerfile -key cosign.pub <path/to/Dockerfile>
+  cosign dockerfile verify -key cosign.pub <path/to/Dockerfile>
 
   # verify images with public key provided by URL
-  cosign verify-dockerfile -key https://host.for/<FILE> <path/to/Dockerfile>
+  cosign dockerfile verify -key https://host.for/<FILE> <path/to/Dockerfile>
 
   # verify images with public key stored in Azure Key Vault
-  cosign verify-dockerfile -key azurekms://[VAULT_NAME][VAULT_URI]/[KEY] <path/to/Dockerfile>
+  cosign dockerfile verify -key azurekms://[VAULT_NAME][VAULT_URI]/[KEY] <path/to/Dockerfile>
 
   # verify images with public key stored in AWS KMS
-  cosign verify-dockerfile -key awskms://[ENDPOINT]/[ID/ALIAS/ARN] <path/to/Dockerfile>
+  cosign dockerfile verify -key awskms://[ENDPOINT]/[ID/ALIAS/ARN] <path/to/Dockerfile>
 
   # verify images with public key stored in Google Cloud KMS
-  cosign verify-dockerfile -key gcpkms://projects/[PROJECT]/locations/global/keyRings/[KEYRING]/cryptoKeys/[KEY] <path/to/Dockerfile>
+  cosign dockerfile verify -key gcpkms://projects/[PROJECT]/locations/global/keyRings/[KEYRING]/cryptoKeys/[KEY] <path/to/Dockerfile>
 
   # verify images with public key stored in Hashicorp Vault
-  cosign verify-dockerfile -key hashivault://[KEY] <path/to/Dockerfile>`,
+  cosign dockerfile verify -key hashivault://[KEY] <path/to/Dockerfile>`,
 
 		FlagSet: flagset,
 		Exec:    cmd.Exec,
