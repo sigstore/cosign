9b2c97dec295754cddf7e4a03743f41926348293
Scott Nichols <deoryp@gmail.com>
2021-09-20 08:26:50 -0700

    Migrate ffcli flags to POSIX with a warning message.
    Signed-off-by: Scott Nichols <deoryp@gmail.com>

--- cmd/sget/main.go
+++ cmd/sget/main.go
@@ -16,12 +16,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/sigstore/cosign/cmd/sget/cli"
 )
 
 func main() {
+	// Fix up flags to POSIX standard flags.
+	for i, arg := range os.Args {
+		if (strings.HasPrefix(arg, "-") && len(arg) == 2) || (strings.HasPrefix(arg, "--") && len(arg) >= 4) {
+			continue
+		}
+		if strings.HasPrefix(arg, "--") && len(arg) == 3 {
+			// Handle --o, convert to -o
+			newArg := fmt.Sprintf("-%c", arg[2])
+			fmt.Fprintf(os.Stderr, "WARNING: the flag %s is deprecated and will be removed in a future release. Please use the flag %s.\n", arg, newArg)
+			os.Args[i] = newArg
+		} else if strings.HasPrefix(arg, "-") {
+			// Handle -output, convert to --output
+			newArg := fmt.Sprintf("-%s", arg)
+			fmt.Fprintf(os.Stderr, "WARNING: the flag %s is deprecated and will be removed in a future release. Please use the flag %s.\n", arg, newArg)
+			os.Args[i] = newArg
+		}
+	}
+
 	if err := cli.New().Execute(); err != nil {
 		log.Fatalf("error during command execution: %v", err)
 	}
