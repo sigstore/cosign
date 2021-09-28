sigstore/cosign

commit 59a62004d5c9f8cfd302438531ef3fb22b52a777

Change `UploadFiles` to return a `name.Digest`. (#795)
Previously this defined and returned a `Digester`, and constructed the digest using the ref that was passed in.  It simplifies things a bunch to just have `UploadFiles` return the digest of the thing it uploaded.

Signed-off-by: Matt Moore <mattomata@gmail.com>

pkg/cosign/remote/index.go
@@ -22,7 +22,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/empty"
@@ -31,10 +30,6 @@ import (
 	"github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/sigstore/cosign/pkg/oci/static"
 )
-
-type Digester interface {
-	Digest() (v1.Hash, error)
-}
 
 type File interface {
 	Contents() ([]byte, error)
@@ -95,31 +90,30 @@ func DefaultMediaTypeGetter(b []byte) types.MediaType {
 	return types.MediaType(strings.Split(http.DetectContentType(b), ";")[0])
 }
 
-func UploadFiles(ref name.Reference, files []File, getMt MediaTypeGetter, remoteOpts ...remote.Option) (Digester, error) {
-	var img v1.Image
+func UploadFiles(ref name.Reference, files []File, getMt MediaTypeGetter, remoteOpts ...remote.Option) (name.Digest, error) {
+	var lastHash v1.Hash
 	var idx v1.ImageIndex = empty.Index
 
 	for _, f := range files {
 		b, err := f.Contents()
 		if err != nil {
-			return nil, err
+			return name.Digest{}, err
 		}
 		mt := getMt(b)
 		fmt.Fprintf(os.Stderr, "Uploading file from [%s] to [%s] with media type [%s]\n", f.Path(), ref.Name(), mt)
-		_img, err := UploadFile(b, ref, mt, types.OCIConfigJSON, remoteOpts...)
+		img, err := UploadFile(b, ref, mt, types.OCIConfigJSON, remoteOpts...)
 		if err != nil {
-			return nil, err
+			return name.Digest{}, err
 		}
-		img = _img
-		l, err := _img.Layers()
+		l, err := img.Layers()
 		if err != nil {
-			return nil, err
+			return name.Digest{}, err
 		}
-		dgst, err := l[0].Digest()
+		lastHash, err = l[0].Digest()
 		if err != nil {
-			return nil, err
+			return name.Digest{}, err
 		}
-		blobURL := ref.Context().Registry.RegistryStr() + "/v2/" + ref.Context().RepositoryStr() + "/blobs/sha256:" + dgst.Hex
+		blobURL := ref.Context().Registry.RegistryStr() + "/v2/" + ref.Context().RepositoryStr() + "/blobs/" + lastHash.String()
 		fmt.Fprintf(os.Stderr, "File [%s] is available directly at [%s]\n", f.Path(), blobURL)
 		if f.Platform() != nil {
 			idx = mutate.AppendManifests(idx, mutate.IndexAddendum{
@@ -132,12 +126,16 @@ func UploadFiles(ref name.Reference, files []File, getMt MediaTypeGetter, remote
 	}
 
 	if len(files) > 1 {
-		if err := remote.WriteIndex(ref, idx, remote.WithAuthFromKeychain(authn.DefaultKeychain)); err != nil {
-			return nil, err
+		err := remote.WriteIndex(ref, idx, remoteOpts...)
+		if err != nil {
+			return name.Digest{}, err
 		}
-		return idx, nil
+		lastHash, err = idx.Digest()
+		if err != nil {
+			return name.Digest{}, err
+		}
 	}
-	return img, nil
+	return ref.Context().Digest(lastHash.String()), nil
 }
 
 func UploadFile(b []byte, ref name.Reference, layerMT, configMt types.MediaType, remoteOpts ...remote.Option) (v1.Image, error) {

cmd/cosign/cli/upload/blob.go
@@ -102,19 +102,13 @@ func BlobCmd(ctx context.Context, regOpts options.RegistryOpts, files []cremote.
 		return err
 	}
 
-	dgster, err := cremote.UploadFiles(ref, files, cremote.DefaultMediaTypeGetter, regOpts.GetRegistryClientOpts(ctx)...)
+	dgstAddr, err := cremote.UploadFiles(ref, files, cremote.DefaultMediaTypeGetter, regOpts.GetRegistryClientOpts(ctx)...)
 	if err != nil {
 		return err
 	}
-	if dgster == nil {
-		return errors.New("dgstr is nil, no files uploaded?")
+	if len(files) == 0 {
+		return errors.New("no files uploaded?")
 	}
-	dgst, err := dgster.Digest()
-	if err != nil {
-		return err
-	}
-	dgstAddr := ref.Context().Digest(dgst.String())
-
 	if len(files) > 1 {
 		fmt.Fprintf(os.Stderr, "Uploading multi-platform index to %s\n", dgstAddr)
 	} else {

commit 6e68bdfcd771934c881721e4071d41bdb716ebe6

Fix the postsubmit failure introduced by #795
The logs of a failure are here: https://github.com/sigstore/cosign/runs/3705988573#step:5:492

The problem here is that when I changed the return value to be the `name.Digest`, I was setting `lastHash` to the `v1.Layer` hash, not the `v1.Image` hash, so when the post-submit job tried to sign the resulting digest things exploded.

This change properly sets `lastHash` to the `v1.Image` hash, and uses a separate `layerHash` variable for the layer's digest for the download print statement we print.

Signed-off-by: Matt Moore <mattomata@gmail.com>

pkg/cosign/remote/index.go
@@ -106,6 +106,10 @@ func UploadFiles(ref name.Reference, files []File, getMt MediaTypeGetter, remote
 		if err != nil {
 			return name.Digest{}, err
 		}
+		lastHash, err = img.Digest()
+		if err != nil {
+			return name.Digest{}, err
+		}
 		if err := remote.Write(ref, img, remoteOpts...); err != nil {
 			return name.Digest{}, err
 		}
@@ -113,11 +117,11 @@ func UploadFiles(ref name.Reference, files []File, getMt MediaTypeGetter, remote
 		if err != nil {
 			return name.Digest{}, err
 		}
-		lastHash, err = l[0].Digest()
+		layerHash, err := l[0].Digest()
 		if err != nil {
 			return name.Digest{}, err
 		}
-		blobURL := ref.Context().Registry.RegistryStr() + "/v2/" + ref.Context().RepositoryStr() + "/blobs/" + lastHash.String()
+		blobURL := ref.Context().Registry.RegistryStr() + "/v2/" + ref.Context().RepositoryStr() + "/blobs/" + layerHash.String()
 		fmt.Fprintf(os.Stderr, "File [%s] is available directly at [%s]\n", f.Path(), blobURL)
 		if f.Platform() != nil {
 			idx = mutate.AppendManifests(idx, mutate.IndexAddendum{