6d63e6b3859e04b78527b75d2b653935b20ae525
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2021-10-06 10:30:57 +0300

    feat: add github/gitlab to generate-key-pair command's description
    Signed-off-by: Batuhan Apaydın <batuhan.apaydin@trendyol.com>

--- cmd/cosign/cli/generate_key_pair.go
+++ cmd/cosign/cli/generate_key_pair.go
@@ -49,6 +49,12 @@ func addGenerateKeyPair(topLevel *cobra.Command) {
   # generate a key-pair in Kubernetes Secret
   cosign generate-key-pair k8s://[NAMESPACE]/[NAME]
 
+  # generate a key-pair in GitHub
+  cosign generate-key-pair github://owner/project
+
+  # generate a key-pair in GitLab
+  cosign generate-key-pair gitlab://owner/project
+
 CAVEATS:
   This command interactively prompts for a password. You can use
   the COSIGN_PASSWORD environment variable to provide one.`,
