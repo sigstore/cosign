0c23825642e799de24d60bd218f657d9c5cd6ea3
Jake Sanders <jsand@google.com>
2021-11-10 11:56:43 -0800

    make the purpose of secrets checked into `.github/workflows` explicit
    Signed-off-by: Jake Sanders <jsand@google.com>

--- test/ci.mk
+++ test/ci.mk
@@ -4,12 +4,12 @@
 
 .PHONY: sign-container
 sign-container: ko
-	cosign sign --key .github/workflows/cosign.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/cosign:$(GIT_HASH)
+	cosign sign --key .github/workflows/cosign-test.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/cosign:$(GIT_HASH)
 
 .PHONY: sign-cosigned
 sign-cosigned:
-	cosign sign --key .github/workflows/cosign.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/cosigned:$(GIT_HASH)
+	cosign sign --key .github/workflows/cosign-test.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/cosigned:$(GIT_HASH)
 
 .PHONY: sign-sget
 sign-sget:
-	cosign sign --key .github/workflows/cosign.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/sget:$(GIT_HASH)
+	cosign sign --key .github/workflows/cosign-test.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/sget:$(GIT_HASH)
