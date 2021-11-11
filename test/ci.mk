62086fe943a6416a6ce77d9ee7d4de719c29e1ad
Carlos Panato <ctadeu@gmail.com>
2021-11-11 12:58:30 +0100

    ci: update oidc ci tests
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- test/ci.mk
+++ test/ci.mk
@@ -13,3 +13,21 @@ sign-cosigned:
 .PHONY: sign-sget
 sign-sget:
 	cosign sign --key .github/workflows/cosign-test.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/sget:$(GIT_HASH)
+
+.PHONY: sign-keyless-cosign
+sign-keyless-cosign:
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/cosign:$(GIT_HASH)
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/cosign:$(GIT_VERSION)
+
+.PHONY: sign-keyless-cosigned
+sign-keyless-cosigned:
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/cosigned:$(GIT_HASH)
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/cosigned:$(GIT_VERSION)
+
+.PHONY: sign-keyless-sget
+sign-keyless-sget:
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/sget:$(GIT_HASH)
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/sget:$(GIT_VERSION)
+
+.PHONY: sign-keyless-container
+sign-keyless-container: ko sign-keyless-cosign sign-keyless-cosigned sign-keyless-sget
