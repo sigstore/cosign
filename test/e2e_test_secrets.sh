d0d91abfa8f9c9cc7dde6e36b837cd72b8f3c11e
Jake Sanders <jsand@google.com>
2021-12-08 20:53:41 -0800

    add test for interactive private key password prompt (#1176)
    Signed-off-by: Jake Sanders <jsand@google.com>

--- test/e2e_test_secrets.sh
+++ test/e2e_test_secrets.sh
@@ -166,5 +166,13 @@ export COSIGN_REPOSITORY=${TEST_INSTANCE_REPO}/subbedrepo
 ./cosign verify --key ${verification_key} $img
 unset COSIGN_REPOSITORY
 
+# test stdin interaction for private key password
+stdin_password=${COSIGN_PASSWORD}
+unset COSIGN_PASSWORD
+(crane delete $(./cosign triangulate $img)) || true
+echo $stdin_password | ./cosign sign --key ${signing_key} --output-signature interactive.sig  $img
+./cosign verify --key ${verification_key} --signature interactive.sig $img
+export COSIGN_PASSWORD=${stdin_password}
+
 # What else needs auth?
 echo "SUCCESS"
