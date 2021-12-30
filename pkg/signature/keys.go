24e7e6a3f628b5dfbd8af483f627c6930ecfeac6
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2021-12-30 12:45:31 +0300

    feat: log error to stderr
    Signed-off-by: Batuhan Apaydın <batuhan.apaydin@trendyol.com>

--- pkg/signature/keys.go
+++ pkg/signature/keys.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"crypto"
 	"crypto/x509"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -140,12 +141,14 @@ func SignerVerifierFromKeyRef(ctx context.Context, keyRef string, pf cosign.Pass
 		}
 
 		return cosign.LoadECDSAPrivateKey([]byte(pk), []byte(pass))
-	default:
-		if sv, err := kms.Get(ctx, keyRef, crypto.SHA256); err == nil {
-			return sv, nil
-		}
 	}
 
+	sv, err := kms.Get(ctx, keyRef, crypto.SHA256)
+	if err == nil {
+		return sv, nil
+	}
+
+	fmt.Fprintf(os.Stderr, "an error occurred: %v, will try to load key from disk...\n", err)
 	return loadKey(keyRef, pf)
 }
 
