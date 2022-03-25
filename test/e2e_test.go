4f0cc3d2bd6451a7ab87851bfbe83332729793fe
Hayden Blauzvern <hblauzvern@google.com>
2022-03-25 05:51:14 +0000

    Fix test
    Signed-off-by: Hayden Blauzvern <hblauzvern@google.com>

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -540,7 +540,7 @@ func TestSignBlobBundle(t *testing.T) {
 		BundlePath: bundlePath,
 	}
 	// Verify should fail on a bad input
-	mustErr(cliverify.VerifyBlobCmd(ctx, ko1, "", "", "", "", blob), t)
+	mustErr(cliverify.VerifyBlobCmd(ctx, ko1, "", "", "", "", "", blob), t)
 
 	// Now sign the blob with one key
 	ko := sign.KeyOpts{
@@ -553,7 +553,7 @@ func TestSignBlobBundle(t *testing.T) {
 		t.Fatal(err)
 	}
 	// Now verify should work
-	must(cliverify.VerifyBlobCmd(ctx, ko1, "", "", "", "", bp), t)
+	must(cliverify.VerifyBlobCmd(ctx, ko1, "", "", "", "", "", bp), t)
 
 	// Now we turn on the tlog and sign again
 	defer setenv(t, options.ExperimentalEnv, "1")()
@@ -563,7 +563,7 @@ func TestSignBlobBundle(t *testing.T) {
 
 	// Point to a fake rekor server to make sure offline verification of the tlog entry works
 	os.Setenv(serverEnv, "notreal")
-	must(cliverify.VerifyBlobCmd(ctx, ko1, "", "", "", "", bp), t)
+	must(cliverify.VerifyBlobCmd(ctx, ko1, "", "", "", "", "", bp), t)
 }
 
 func TestGenerate(t *testing.T) {
