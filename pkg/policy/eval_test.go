f7efb9d10b3de7251c37d9549e832bf39c1211d8
Ville Aikas <vaikas@chainguard.dev>
2022-04-21 15:04:31 -0700

    loving yaml, thanks validation!
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>

--- pkg/policy/eval_test.go
+++ pkg/policy/eval_test.go
@@ -26,7 +26,8 @@ const (
 
 	vulnAttestation = "{\"_type\":\"https://in-toto.io/Statement/v0.1\",\"predicateType\":\"cosign.sigstore.dev/attestation/vuln/v1\",\"subject\":[{\"name\":\"registry.local:5000/cosigned/demo\",\"digest\":{\"sha256\":\"416cc82c76114b1744ea58bcbf2f411a0f2de4b0456703bf1bb83d33656951bc\"}}],\"predicate\":{\"invocation\":{\"parameters\":null,\"uri\":\"invocation.example.com/cosign-testing\",\"event_id\":\"\",\"builder.id\":\"\"},\"scanner\":{\"uri\":\"fakescanner.example.com/cosign-testing\",\"version\":\"\",\"db\":{\"uri\":\"\",\"version\":\"\"},\"result\":null},\"metadata\":{\"scanStartedOn\":\"2022-04-12T00:00:00Z\",\"scanFinishedOn\":\"2022-04-12T00:10:00Z\"}}}"
 
-	cipAttestation = "{\"authorityMatches\":{\"key-att\":{\"signatures\":null,\"attestations\":{\"vuln-key\":[{\"subject\":\"PLACEHOLDER\",\"issuer\":\"PLACEHOLDER\"}]}},\"key-signature\":{\"signatures\":[{\"subject\":\"PLACEHOLDER\",\"issuer\":\"PLACEHOLDER\"}],\"attestations\":null},\"keyless-att\":{\"signatures\":null,\"attestations\":{\"custom-keyless\":[{\"subject\":\"PLACEHOLDER\",\"issuer\":\"PLACEHOLDER\"}]}}"
+	// TODO(vaikas): Enable tests once we sort this out.
+	//	cipAttestation = "{\"authorityMatches\":{\"key-att\":{\"signatures\":null,\"attestations\":{\"vuln-key\":[{\"subject\":\"PLACEHOLDER\",\"issuer\":\"PLACEHOLDER\"}]}},\"key-signature\":{\"signatures\":[{\"subject\":\"PLACEHOLDER\",\"issuer\":\"PLACEHOLDER\"}],\"attestations\":null},\"keyless-att\":{\"signatures\":null,\"attestations\":{\"custom-keyless\":[{\"subject\":\"PLACEHOLDER\",\"issuer\":\"PLACEHOLDER\"}]}}"
 )
 
 func TestEvalPolicy(t *testing.T) {
@@ -81,10 +82,8 @@ func TestEvalPolicy(t *testing.T) {
 		if tc.wantErr {
 			if err == nil {
 				t.Errorf("Did not get an error, wanted %s", tc.wantErrSub)
-			} else {
-				if !strings.Contains(err.Error(), tc.wantErrSub) {
-					t.Errorf("Unexpected error, want: %s got: %s", tc.wantErrSub, err.Error())
-				}
+			} else if !strings.Contains(err.Error(), tc.wantErrSub) {
+				t.Errorf("Unexpected error, want: %s got: %s", tc.wantErrSub, err.Error())
 			}
 		} else {
 			if !tc.wantErr && err != nil {
