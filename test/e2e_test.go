fee13709de7b798acc1d274a82b4f446feff42fb
Akira Saso <sasoakira6114@gmail.com>
2022-08-16 13:07:05 +0900

    test: fix cue used in verify-attstation
    Signed-off-by: Akira Saso <sasoakira6114@gmail.com>

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -212,8 +212,8 @@ func TestAttestVerify(t *testing.T) {
 	attestVerify(t,
 		"slsaprovenance",
 		`{ "buildType": "x", "builder": { "id": "2" }, "recipe": {} }`,
-		`builder: id: "1"`,
-		`builder: id: "2"`,
+		`predicate: builder: id: "2"`,
+		`predicate: builder: id: "1"`,
 	)
 }
 
@@ -225,8 +225,8 @@ func TestAttestVerifySPDXJSON(t *testing.T) {
 	attestVerify(t,
 		"spdxjson",
 		string(attestationBytes),
-		`Data: spdxVersion: "SPDX-9.9"`,
-		`Data: spdxVersion: "SPDX-2.2"`,
+		`predicate: Data: spdxVersion: "SPDX-2.2"`,
+		`predicate: Data: spdxVersion: "SPDX-9.9"`,
 	)
 }
 
@@ -238,8 +238,8 @@ func TestAttestVerifyCycloneDXJSON(t *testing.T) {
 	attestVerify(t,
 		"cyclonedx",
 		string(attestationBytes),
-		`Data: specVersion: "7.7"`,
-		`Data: specVersion: "1.4"`,
+		`predicate: Data: specVersion: "1.4"`,
+		`predicate: Data: specVersion: "7.7"`,
 	)
 }
 
@@ -304,6 +304,7 @@ func attestVerify(t *testing.T, predicateType, attestation, goodCue, badCue stri
 	if err := os.WriteFile(policyPath, []byte(badCue), 0600); err != nil {
 		t.Fatal(err)
 	}
+	mustErr(verifyAttestation.Exec(ctx, []string{imgName}), t)
 
 	// Success case
 	if err := os.WriteFile(policyPath, []byte(goodCue), 0600); err != nil {
@@ -1039,7 +1040,7 @@ func TestSaveLoadAttestation(t *testing.T) {
 	verifyAttestation.PredicateType = "slsaprovenance"
 	verifyAttestation.Policies = []string{policyPath}
 	// Success case (remote)
-	cuePolicy := `builder: id: "2"`
+	cuePolicy := `predicate: builder: id: "2"`
 	if err := os.WriteFile(policyPath, []byte(cuePolicy), 0600); err != nil {
 		t.Fatal(err)
 	}
