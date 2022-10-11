e65cf3d8e27c5dc6797fd02ccc814ee82a73636a
Fredrik Skogman <kommendorkapten@github.com>
2022-10-11 15:08:17 +0200

    Updated the verification pseudo algorithm to match the updated bundle format.
    Signed-off-by: Fredrik Skogman <kommendorkapten@github.com>

--- specs/dotsigstore_bundle/verifier.go
+++ specs/dotsigstore_bundle/verifier.go
@@ -1,5 +1,9 @@
 // Pseudo-code for an example verification flow for a sigstore bundle,
 // with some input parameters required from the client.
+// This algorighm is written without any dependency on TUF, this implies
+// that any client must *FIRST* resolve any verification materials (root
+// certificates, public keys etc) and provide them upfront the Verify
+// method.
 
 type Verifier interface{
 	// Verify verifier the provided data against the signatre,
@@ -7,37 +11,78 @@ type Verifier interface{
 	Verify(data, sig []byte, kid string) error
 }
 
+type LogId []byte
 
 type Options {
 	// Verifier for user provided keys. May know about many keys.
+	// The 'Verifier' verifies the signature of either the DSSE envelope
+	// or the MessageSignature.
 	Verifier Verifier
-	// Optional user provided Root CA
-	RootCA *x509.Certificate
-	// Optional Rekor URL
-	RekorURL string
-	// Optional Fulcio URL
-	FulcioURL string
+	// User provided root certificate for verifying certificate chain.
+	RootCertificate *x509.Certificate
+	// Map of trusted root certificates when verifying RFC3161 signed
+	// timestamps. Indexex by their 'Name'.
+	Rfc3162RootCertificates map[pkix.Name]*x509.Certificate
+	// Map of trusted public keys for verifying a transparency log entry.
+	// The keys are indexed by the log's id.
+	// PublicKey is expected to be the concrete representation of the
+	// public key, with all parameters required (padding, hash function
+	// etc.) instantiated.
+	RekorPublicKeys map[LogId]*PublicKey
+	// Set to true if the transparency log entry's inclusion promise
+	// (SET) is not enough, and a full inclsion proof is required.
+	RequireInclusionProof bool
 }
 
 // Verify is the main verirication function.
 // It accepts a required sigstore bundle, and an optional path to the blob.
 // In if the attestation in the bundle is a DSSE envelope, the blob path
 // is ignored.
-func Verify(b *bundle, blob string, opts Options) error {
-	if b.AttestationDsse != nil {
+// All cryptographic verification materials such as root certificates *MUST*
+// be provided to this method.
+// This function only verifies the signatures (including time of signature
+// creation when using a certificate), root of trust and that the
+// signatures are for the provided artifact. It does not parse the content
+// in the DSSE envelope (if provided). Clients are expected to rely on this
+// function to verify the authenticity of the artifact, and then resort to
+// domain specific validation functions.
+// Providing a bundle which contains a DSSE envelope and a path to a blob is
+// an error.
+func Verify(b *bundle, blobPath string, opts Options) error {
+	if b.GetDsseEnvelope() != nil {
+		if blobPath != nil {
+			return ErrBadInput
+		}
+
 		if err = verifyDsse(b, opts); err != nil {
 			return err
 		}
 	} else {
-		if err = verifyBlob(b, blob, opts); err != nil {
+		if err = verifyBlob(b, blobPath, opts); err != nil {
 			return err
 		}
 	}
 
-	if b.rekorEntry != nil {
-		err = verifyRekor(b, blob, opts); err != nil {
+	var verifiedOk = 0
+	for entry = range b.TimestampVerificationData.GetTLogEntries() {
+		err = verifyRekor(b, entry, blobPath, opts); err != nil {
 			return err
 		}
+		verifiedOk++
+	}
+	if verifiedOk < opts.TLogThreshold {
+		return ErrTooFewTlogs
+	}
+
+	verifiedOk = 0
+	for ts = range b.TimestampVerificationData.GetRfc3161Timestamps() {
+		err = verifyRfc3161Ts(b, ts, blobPath, opts); err != nil {
+			return err
+		}
+		verifiedOk++
+	}
+	if verifiedOk < opts.TimestampThreshold {
+		return ErrTooFewTimestamps
 	}
 
 	return nil
@@ -46,70 +91,141 @@ func Verify(b *bundle, blob string, opts Options) error {
 // Extract the verifier to use, either based on a set of provided keys, or
 // a complete X509 chain.
 func getVerifier(b *bundle, opts Options) Verifier {
-	if b.publicKey {
-		return = opts.Verifier
+	var chain = b.VerificationMaterial.GetX509CertificateChain()
+
+	if chain == nil {
+		// The verification material is a public key, return the
+		// user provided verifier, which contains the set of trusted
+		// public keys.
+		return opts.Verifier
 	} else {
-		return = x509Verifier(b.X509Cert, opts)
+		// Generic verifier that verifies:
+		// * the provided signature is correct given the data and
+		//   certificate (leaf certificate in the certificate chain).
+		// * The entire certificate chain is consistent up to the
+		//   root certificate, *and* that the root certificate
+		//   matches the user provided.
+		return x509Verifier(chain, opts.RootCertificate)
 	}
 }
 
 // Standard DSSE verification using the provided verification material.
 func verifyDsse(b *bundle, opts options) error {
 	verifier = getVerifier(b, opts)
-	return stdDsseVerification(b.AttestationDsse, verifier)
+	// stdDsseVerification shall implement the first three steps of
+	// DSSE verification as defined here: https://github.com/secure-systems-lab/dsse/blob/master/protocol.md#dsse-protocol
+	// Verification of PAYLOAD_TYPE is expected to be performed after
+	// bundle verification is done.
+	return stdDsseVerification(b.GetDsseEnvelope(), verifier)
 }
 
-// Verify a blob attestation by first comparing the payloaddigest, and the
-// the signature over the digest using the provided verification material.
+// Verify a blob's message signature.
+// First the message digest is calculated to make sure the provided blob
+// is the one being referenced.
+// Then the signature is verified by the provided crypto material
 func verifyBlob(b *bundle, blob string, opts Options) error {
-	digest = calculateDigest(blob, b.AttestationBlob.Algorithm)
-	if digest != b.AttestationBlob.PayloadHash {
-		return digestMismatch
-	}
-	verifier = getVerifier(b, opts)
-	data = io.ReadAll(blob)
-	return verifier.Verify(data, b.AttestationBlob.Signature, b.PublicKey.KeyId || "")
-}
+	var signature = b.GetMessageSignature()
+	var digest = calculateDigest(blob, signature.MessageDigest.Algorithm)
 
-// Load an X509 verifier based on the options. Either the default Fulcio CA
-// is used, or a CA provided by the user. The returned verifier contains
-// both the leaf certificate (signing cert) and the entire chain, including
-// the CA which was provided out of bands.
-func x509Verifier(x509 X509CertStruct, opts Options) Verifier {
-	if opts.RootCA == nil {
-		return loadCAFromTuf(opts.FulcioURL)
+	if digest != signature.MessageDigest.Digest {
+		return ErrDigestMismatch
 	}
-	// the x509 variable is the struct of cert + chain.
-	return x509ChainVerifier(x509, opts.RootCA)
+	var verifier = getVerifier(b, opts)
+	var data = io.ReadAll(blob)
+	// the keyHint is empty if the verification material is a X509 cert,
+	// the prepared verifier from getVerifier is correctly configured
+	// though.
+	var keyHint = b.VerificationMaterial.GetPublicKey()
+	return verifier.Verify(data, signature.Signature, keyHint)
 }
 
 // Verify that the rekor entry matches the provided attestation.
 // As the rekor entry is partially provided, and is composed based on data
 // in the sigstore bundle, the verification always makes sure that the rekor
 // entry was for the provided sigsture bundle.
-func verifyRekor(b *bundle, blob string, opts options) error {
-	entry = reconstructRekorEntry(b, d)
-	RekorPubKey = loadFromTuf(opts.RekorURL)
+func verifyRekor(b *bundle, tlogEntry *TransparencyLogEntry, blob string, opts options) error {
+	var entry = reconstructRekorEntry(b, tlogEntry, blob)
+	var rekorPubKey = opts.RekorPublicKeys[tlogEntry.LogId]
 
-	return verifySET(entry, b.rekorEntry.SignedEntryTimestamp, o.RekorPubKey)
+	// Verify that the inclusion time is within the validity of the
+	// certificate. This should be done by the transparency log prior
+	// to appending the entry to the log. But there is no harm to verify
+	// it once more.
+	var chain = b.VerificationMaterial.GetX509CertificateChain()
+	if chain != nil && !validCertificateChain(chain, tlogEntry.IntegrationTime) {
+		return ErrCertificateExpired
+	}
+
+	if !verifySET(entry, tlogEntry.InclusionPromise, rekorPubKey) {
+		return ErrSETMismatch
+	}
+
+	if opts.RequireInclusionProof {
+		// Perform an online verification against the Rekor instance
+		var client = getRekorClient(tlogEntry.LogId)
+		return client.ProveInclusion(b, tlogEntry, blob, opts)
+	}
+
+	return nil
 }
 
 // Reconstruct the Rekor type by introspecting what kind of attestation
 // type is provided.
-func reconstructRekroEntry(b *bundle, blob string) *rekorEntry{
-	switch b.RekorEntry.Kind {
+func reconstructRekroEntry(b *bundle, entry *TransparencyLogEntry, blob string) *rekorEntry{
+	switch entry.KindVersion.Kind {
 	case intoto:
 		return rekorTypeIntoto(
-			b.RekorEntry.logIndex,
-			b.RekorEntry.logId,
+			entry.logIndex,
+			entry.logId,
 			...
-			b.AttestationDsse)
+			b.GetDsseEnvelope())
 	case hashedrecord:
+		var signature = b.GetMessageSignature()
 		return rekorTypeRecordHash(
-			b.RekorEntry.logIndex,
-			b.RekorEntry.logId,
+			entry.logIndex,
+			entry.logId,
 			...
-			b.AttestationBlob.Signature,
-			calculateDigest(blob, b.AttestationBlob.Algorithm))
+			signature.Signature,
+			calculateDigest(blob, signature.MessageDigest.Algorithm))
 	}
+}
+
+// Verify that a provided RFC3161 signed timestamp matches the provided
+// bundle.
+// The argument ts is here assumed to be a TimeStampToken as defined
+// here https://www.ietf.org/rfc/rfc3161.html#section-2.4.2.
+// Implementation may opt to accet the raw DER bytes, and call out to an
+// appropriate library.
+func verifyRfc3161Ts(b *bundle, tst TimestampToken, blob string, opts Options) error {
+	// Verify that the TSA is trusted
+	if !verifyCertChain(tst.Content.Certificates, opts.Rfc3162RootCertificates) {
+		return ErrCertChainMisMatch
+	}
+	// Verify that the timestamp token is valid
+	if !rfc3162.Verify(ts) {
+		return ErrInvalidTimestamp
+	}
+	// Recalculate the message digest and compare with the value in the
+	// timestamp token. Note that the hash algorithm in the timestamp
+	// token may differ from the one in the message signature.
+	var alg = tst.TstInfo.MessageImprint.HashAlgorithm
+	var digest = tst.TstInfo.MessageImprint.HashedMessage
+	var data []byte
+	if b.GetDsseEnvelope() != nil {
+		data = b.GetDsseEnvelope().Payload
+	} else {
+		data = io.ReadAll(blob)
+	}
+	if digest != calculateDigest(data, alg) {
+		return ErrDigestMismatch
+	}
+
+	// Verify that the signature was created during the time the
+	// certificate was valid
+	var chain = b.VerificationMaterial.GetX509CertificateChain()
+	if chain != nil && !validCertificateChain(chain, tst.TstInfo.GenTime)
+		return ErrCertificateExpired
+	}
+
+	return nil
 }
