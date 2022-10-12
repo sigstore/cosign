7c2d5cee055bb9000c586e831f93da8b15f51164
Priya Wadhwa <priya@chainguard.dev>
2022-10-12 16:01:50 -0700

    Remove experimental mode from attest-blob
    Signed-off-by: Priya Wadhwa <priya@chainguard.dev>

--- cmd/cosign/cli/attest_blob.go
+++ cmd/cosign/cli/attest_blob.go
@@ -28,10 +28,7 @@ func AttestBlob() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "attest-blob",
 		Short: "Attest the supplied blob.",
-		Example: `  cosign attest-blob --key <key path>|<kms uri> [--predicate <path>] [--a key=value] [--no-upload=true|false] [--f] [--r] <BLOB uri>
-
-  # attach an attestation to a blob Google sign-in (experimental)
-  COSIGN_EXPERIMENTAL=1 cosign attest-blob --timeout 90s --predicate <FILE> --type <TYPE> <BLOB>
+		Example: `  cosign attest-blob --key <key path>|<kms uri> [--predicate <path>] [--a key=value] [--f] [--r] <BLOB uri>
 
   # attach an attestation to a blob with a local key pair file
   cosign attest-blob --predicate <FILE> --type <TYPE> --key cosign.key <BLOB>
@@ -50,27 +47,15 @@ func AttestBlob() *cobra.Command {
 
 		Args: cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			oidcClientSecret, err := o.OIDC.ClientSecret()
-			if err != nil {
-				return err
-			}
 			ko := options.KeyOpts{
-				KeyRef:                   o.Key,
-				PassFunc:                 generate.GetPass,
-				Sk:                       o.SecurityKey.Use,
-				Slot:                     o.SecurityKey.Slot,
-				FulcioURL:                o.Fulcio.URL,
-				IDToken:                  o.Fulcio.IdentityToken,
-				InsecureSkipFulcioVerify: o.Fulcio.InsecureSkipFulcioVerify,
-				RekorURL:                 o.Rekor.URL,
-				OIDCIssuer:               o.OIDC.Issuer,
-				OIDCClientID:             o.OIDC.ClientID,
-				OIDCClientSecret:         oidcClientSecret,
-				OIDCRedirectURL:          o.OIDC.RedirectURL,
+				KeyRef:   o.Key,
+				PassFunc: generate.GetPass,
+				Sk:       o.SecurityKey.Use,
+				Slot:     o.SecurityKey.Slot,
 			}
 			for _, artifact := range args {
-				if err := attest.AttestBlobCmd(cmd.Context(), ko, artifact, o.Hash, o.Cert, o.CertChain, o.NoUpload,
-					o.Predicate.Path, o.Force, o.Predicate.Type, o.Replace, ro.Timeout, o.OutputSignature); err != nil {
+				if err := attest.AttestBlobCmd(cmd.Context(), ko, artifact, o.Hash, o.Cert, o.CertChain,
+					o.Predicate.Path, o.Predicate.Type, ro.Timeout, o.OutputSignature); err != nil {
 					return errors.Wrapf(err, "attesting %s", artifact)
 				}
 			}
