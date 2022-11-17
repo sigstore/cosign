6b309df06f60ea5f58db22e9890713138c823d27
Tom Meadows <thomas.meadows@jetstack.io>
2022-11-17 00:24:26 +0000

    Better feedback if specifying incompatible argument on `cosign sign --attachment` (#2449)
    * gives better feedback if specifying incompatible argument
    
    Signed-off-by: chaosinthecrd <thomas.meadows@jetstack.io>
    
    * removed the fullstop for linting
    
    Signed-off-by: chaosinthecrd <thomas.meadows@jetstack.io>
    
    Signed-off-by: chaosinthecrd <thomas.meadows@jetstack.io>

--- cmd/cosign/cli/sign.go
+++ cmd/cosign/cli/sign.go
@@ -16,7 +16,6 @@
 package cli
 
 import (
-	"flag"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -86,7 +85,7 @@ race conditions or (worse) malicious tampering.
 			case "sbom", "":
 				break
 			default:
-				return flag.ErrHelp
+				return fmt.Errorf("specified image attachment %s not specified. Can be 'sbom'", o.Attachment)
 			}
 			oidcClientSecret, err := o.OIDC.ClientSecret()
 			if err != nil {
