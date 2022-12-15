1dff5cebdd200107ca592420a1ca7d7780531dbd
Ville Aikas <vaikas@chainguard.dev>
2022-12-15 11:12:59 -0800

    forgot to push this file. add blob-verify check.
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>

--- cmd/cosign/cli/verify/verify.go
+++ cmd/cosign/cli/verify/verify.go
@@ -173,9 +173,11 @@ func (c *VerifyCommand) Exec(ctx context.Context, images []string) (err error) {
 	keyRef := c.KeyRef
 	certRef := c.CertRef
 
-	if !c.IgnoreSCT {
-		co.CTLogPubKeys, err = ctl.GetCTLogPubs(ctx)
+	co.CTLogPubKeys, err = ctl.GetCTLogPubs(ctx)
+	if err != nil {
+		return fmt.Errorf("getting ctlog public keys: %w", err)
 	}
+
 	// Keys are optional!
 	var pubKey signature.Verifier
 	switch {
