dd747eeade444d42a5c55c35adf5c17831d60c6c
Tim Seagren <39381000+chaospuppy@users.noreply.github.com>
2023-02-08 06:55:06 -0800

    2476 predicate type download (#2484)
    * add cyclonedxxml predicatetype
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * read cyclonedx xml bytes into cyclonedx.SBOM
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * add e2e test
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * update cli docs
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * add artibrary annotations to mainfest
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * add predicateType annotation to attestation manifests, add --predicate-type option for attestation download
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * remove unrelated changes
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * update PredicateType as field of c
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * apply suggestions
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * remove unused index var
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * add e2e tests for attestation download, resolve data race, update help text for --predicate-type
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    * CreateAttestationReplace
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>
    
    ---------
    
    Signed-off-by: Tim Seagren <seagren.tim@solute.us>

--- pkg/cosign/fetch.go
+++ pkg/cosign/fetch.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"sync"
 
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/sigstore/cosign/v2/pkg/cosign/bundle"
@@ -119,7 +120,7 @@ func FetchSignaturesForReference(ctx context.Context, ref name.Reference, opts .
 	return signatures, nil
 }
 
-func FetchAttestationsForReference(ctx context.Context, ref name.Reference, opts ...ociremote.Option) ([]AttestationPayload, error) {
+func FetchAttestationsForReference(ctx context.Context, ref name.Reference, predicateType string, opts ...ociremote.Option) ([]AttestationPayload, error) {
 	simg, err := ociremote.SignedEntity(ref, opts...)
 	if err != nil {
 		return nil, err
@@ -137,18 +138,44 @@ func FetchAttestationsForReference(ctx context.Context, ref name.Reference, opts
 		return nil, fmt.Errorf("no attestations associated with %s", ref)
 	}
 
-	attestations := make([]AttestationPayload, len(l))
+	attestations := make([]AttestationPayload, 0, len(l))
+	var attMu sync.Mutex
+
 	var g errgroup.Group
 	g.SetLimit(runtime.NumCPU())
-	for i, att := range l {
-		i, att := i, att
+
+	for _, att := range l {
+		if predicateType != "" {
+			anns, err := att.Annotations()
+			if err != nil {
+				return nil, err
+			}
+			pt, ok := anns["predicateType"]
+			// Skip attestation if predicateType annotation is not present or predicateType annotation does not match supplied predicate type
+			if !ok || pt != predicateType {
+				continue
+			}
+		}
+		att := att
+		var a AttestationPayload
 		g.Go(func() error {
 			attestPayload, _ := att.Payload()
-			return json.Unmarshal(attestPayload, &attestations[i])
+			err := json.Unmarshal(attestPayload, &a)
+			if err != nil {
+				return err
+			}
+			attMu.Lock()
+			defer attMu.Unlock()
+			attestations = append(attestations, a)
+			return nil
 		})
 	}
 	if err := g.Wait(); err != nil {
 		return nil, err
+	}
+
+	if len(attestations) == 0 && predicateType != "" {
+		return nil, fmt.Errorf("no attestations with predicate type '%s' found", predicateType)
 	}
 
 	return attestations, nil
