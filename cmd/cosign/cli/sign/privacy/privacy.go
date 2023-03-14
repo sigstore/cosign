0d18d57e565e02dbd272ebbc057aed70822a6c60
Steve Winslow <steve@swinslow.net>
2023-03-14 18:07:37 -0400

    cli: fix linting errors for privacy statement
    Signed-off-by: Steve Winslow <steve@swinslow.net>

--- cmd/cosign/cli/sign/privacy/privacy.go
+++ cmd/cosign/cli/sign/privacy/privacy.go
@@ -22,9 +22,11 @@ const (
 	The sigstore service, hosted by sigstore a Series of LF Projects, LLC, is provided pursuant to the Hosted Project Tools Terms of Use, available at https://lfprojects.org/policies/hosted-project-tools-terms-of-use/.
 	Note that if your submission includes personal data associated with this signed artifact, it will be part of an immutable record.
 	This may include the email address associated with the account with which you authenticate your contractual Agreement.
-	This information will be used for signing this artifact and will be stored in public transparency logs and cannot be removed later, and is subject to the Immutable Record notice at https://lfprojects.org/policies/hosted-project-tools-immutable-records/.`
+	This information will be used for signing this artifact and will be stored in public transparency logs and cannot be removed later, and is subject to the Immutable Record notice at https://lfprojects.org/policies/hosted-project-tools-immutable-records/.
+`
 
-	StatementConfirmation = "By typing 'y', you attest that (1) you are not submitting the personal data of any other person; and (2) you understand and agree to the statement and the Agreement terms at the URLs listed above.")
+	StatementConfirmation = "By typing 'y', you attest that (1) you are not submitting the personal data of any other person; and (2) you understand and agree to the statement and the Agreement terms at the URLs listed above."
+)
 
 var (
 	StatementOnce sync.Once
