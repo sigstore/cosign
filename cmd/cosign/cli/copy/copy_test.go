c17a36a9ce4f6741cf0551e8d767ed1ec9c079bc
Mritunjay Kumar Sharma <mritunjaysharma394@gmail.com>
2023-05-31 20:02:37 +0530

    accepts `attachment-tag-prefix` for `cosign copy` (#3014)
    * accepts attachment-tag-prefix for cosign copy
    
    Signed-off-by: Mritunjay <mritunjaysharma394@gmail.com>
    
    * adds tests for cosign copy attachment-tag-prefix
    
    Signed-off-by: Mritunjay <mritunjaysharma394@gmail.com>
    
    * refactors for prefix logic
    
    Signed-off-by: Mritunjay <mritunjaysharma394@gmail.com>
    
    ---------
    
    Signed-off-by: Mritunjay <mritunjaysharma394@gmail.com>

---
+++ cmd/cosign/cli/copy/copy_test.go
@@ -0,0 +1,40 @@
+// Copyright 2023 the Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package copy
+
+import (
+	"context"
+	"testing"
+
+	"github.com/sigstore/cosign/v2/cmd/cosign/cli/options"
+)
+
+func TestCopyAttachmentTagPrefix(t *testing.T) {
+	ctx := context.Background()
+
+	refOpts := options.ReferenceOptions{
+		TagPrefix: "test-tag",
+	}
+
+	srcImg := "alpine"
+	destImg := "test-alpine"
+
+	err := CopyCmd(ctx, options.RegistryOptions{
+		RefOpts: refOpts,
+	}, srcImg, destImg, false, true)
+	if err == nil {
+		t.Fatal("failed to copy with attachment-tag-prefix")
+	}
+}
