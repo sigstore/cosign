8ac891ff0e29ddc67965423bee8f826219c6eb0f
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2023-11-07 11:59:07 +0000

    Merge pull request from GHSA-vfp6-jrw2-99g9
    * Add limit to number of sigs and attestations
    
    Signed-off-by: AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
    
    * Update pkg/cosign/fetch.go
    
    Co-authored-by: Cody Soyland <codysoyland@gmail.com>
    Signed-off-by: AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
    
    * Update error message
    
    Signed-off-by: Hayden B <hblauzvern@google.com>
    
    * fix compilation error
    
    Signed-off-by: Hayden Blauzvern <hblauzvern@google.com>
    
    * Add e2e tests
    
    Signed-off-by: Hayden Blauzvern <hblauzvern@google.com>
    
    ---------
    
    Signed-off-by: AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
    Signed-off-by: Hayden B <hblauzvern@google.com>
    Signed-off-by: Hayden Blauzvern <hblauzvern@google.com>
    Co-authored-by: Cody Soyland <codysoyland@gmail.com>
    Co-authored-by: Hayden B <hblauzvern@google.com>

--- pkg/cosign/fetch.go
+++ pkg/cosign/fetch.go
@@ -34,6 +34,8 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+const maxAllowedSigsOrAtts = 100
+
 type SignedPayload struct {
 	Base64Signature  string
 	Payload          []byte
@@ -83,6 +85,9 @@ func FetchSignaturesForReference(_ context.Context, ref name.Reference, opts ...
 	}
 	if len(l) == 0 {
 		return nil, fmt.Errorf("no signatures associated with %s", ref)
+	}
+	if len(l) > maxAllowedSigsOrAtts {
+		return nil, fmt.Errorf("maximum number of signatures on an image is %d, found %d", maxAllowedSigsOrAtts, len(l))
 	}
 
 	signatures := make([]SignedPayload, len(l))
@@ -144,6 +149,10 @@ func FetchAttestations(se oci.SignedEntity, predicateType string) ([]Attestation
 	}
 	if len(l) == 0 {
 		return nil, errors.New("found no attestations")
+	}
+	if len(l) > maxAllowedSigsOrAtts {
+		errMsg := fmt.Sprintf("maximum number of attestations on an image is %d, found %d", maxAllowedSigsOrAtts, len(l))
+		return nil, errors.New(errMsg)
 	}
 
 	attestations := make([]AttestationPayload, 0, len(l))
