6b06819a4372319a9c6e4c797500383cd1fc1e07
Yonghe Zhao <yonghe.zhao@yahoo.com>
2023-11-28 03:42:31 -0500

    Use the timeout flag value in verify* commands. (#3391)
    * Use the timeout flag value in verify* commands.
    
    Signed-off-by: zhaoyonghe <yonghe.zhao@yahoo.com>
    
    * Solve "Error: lostcancel: the cancel function returned by context.WithTimeout should be called, not discarded, to avoid a context leak (govet)".
    
    Signed-off-by: zhaoyonghe <yonghe.zhao@yahoo.com>
    
    ---------
    
    Signed-off-by: zhaoyonghe <yonghe.zhao@yahoo.com>

--- cmd/cosign/cli/verify.go
+++ cmd/cosign/cli/verify.go
@@ -16,6 +16,7 @@
 package cli
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/google/go-containerregistry/pkg/name"
@@ -142,7 +143,8 @@ against the transparency log.`,
 				v.NameOptions = append(v.NameOptions, name.Insecure)
 			}
 
-			ctx := cmd.Context()
+			ctx, cancel := context.WithTimeout(cmd.Context(), ro.Timeout)
+			defer cancel()
 
 			if o.CommonVerifyOptions.IgnoreTlog && !o.CommonVerifyOptions.PrivateInfrastructure {
 				ui.Warnf(ctx, fmt.Sprintf(ignoreTLogMessage, "signature"))
@@ -241,7 +243,8 @@ against the transparency log.`,
 				return fmt.Errorf("please set the --max-worker flag to a value that is greater than 0")
 			}
 
-			ctx := cmd.Context()
+			ctx, cancel := context.WithTimeout(cmd.Context(), ro.Timeout)
+			defer cancel()
 
 			if o.CommonVerifyOptions.IgnoreTlog && !o.CommonVerifyOptions.PrivateInfrastructure {
 				ui.Warnf(ctx, fmt.Sprintf(ignoreTLogMessage, "attestation"))
@@ -336,7 +339,8 @@ The blob may be specified as a path to a file or - for stdin.`,
 				IgnoreTlog:                   o.CommonVerifyOptions.IgnoreTlog,
 			}
 
-			ctx := cmd.Context()
+			ctx, cancel := context.WithTimeout(cmd.Context(), ro.Timeout)
+			defer cancel()
 
 			if o.CommonVerifyOptions.IgnoreTlog && !o.CommonVerifyOptions.PrivateInfrastructure {
 				ui.Warnf(ctx, fmt.Sprintf(ignoreTLogMessage, "blob"))
@@ -411,7 +415,8 @@ The blob may be specified as a path to a file.`,
 				path = args[0]
 			}
 
-			ctx := cmd.Context()
+			ctx, cancel := context.WithTimeout(cmd.Context(), ro.Timeout)
+			defer cancel()
 
 			if o.CommonVerifyOptions.IgnoreTlog && !o.CommonVerifyOptions.PrivateInfrastructure {
 				ui.Warnf(ctx, fmt.Sprintf(ignoreTLogMessage, "blob attestation"))
