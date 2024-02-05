7951940299ecc19756f5585e9043faaf7003d4e1
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2024-02-05 21:04:31 +0530

    add e2e test for pkcs11 token signing (#3495)
    * added e2e test for pkcs11 token signing
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    add license
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    small fix
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    update shebang portable with cross platform
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    enable exit on error and xtrace mode
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    cleanup container
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    pkcs11 test with upcoming changes
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    run pkcs11 e2e test in a separate workflow
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    add pkcs11 test in separate workflow
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    * set shell to bash
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    * set shell options
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
    
    ---------
    
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

---
+++ test/e2e_test_pkcs11.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Copyright 2024 The Sigstore Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Test pkcs11 token signing
+CONTAINER_ID=$(docker run -dit --name softhsm -v $(pwd):/root/cosign -p 2345:2345 vegardit/softhsm2-pkcs11-proxy@sha256:557a65d2a14e3986f2389d36ddce75609cbd8fb7ee6cf08a78adcc8236c2a80e)
+
+docker exec -i $CONTAINER_ID /bin/bash << 'EOF'
+
+apk update
+
+# add make pcsc-lite-libs go command
+apk add make build-base go
+
+cd /root/cosign
+
+softhsm2-util --init-token --free --label "My Token" --pin 1234 --so-pin 1234
+go test -v -cover -coverprofile=./cover.out -tags=softhsm,pkcs11key -coverpkg github.com/sigstore/cosign/v2/pkg/cosign/pkcs11key test/pkcs11_test.go
+
+EOF
+
+cleanup_pkcs11() {
+    docker rm -f $CONTAINER_ID
+}
+
+trap cleanup_pkcs11 EXIT
