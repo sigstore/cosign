bfe8d378495819ed5f2579246b63b9c106629d6d
Zach Steindler <steiza@github.com>
2024-07-01 11:14:05 -0400

    Add basic tests for SignBlobCmd
    Signed-off-by: Zach Steindler <steiza@github.com>

---
+++ cmd/cosign/cli/sign/sign_blob_test.go
@@ -0,0 +1,52 @@
+// Copyright 2024 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sign
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/sigstore/cosign/v2/cmd/cosign/cli/options"
+	"github.com/sigstore/cosign/v2/pkg/cosign"
+)
+
+func TestSignBlobCmd(t *testing.T) {
+	td := t.TempDir()
+
+	keys, _ := cosign.GenerateKeyPair(nil)
+	keyRef := writeFile(t, td, string(keys.PrivateBytes), "key.pem")
+
+	blob := []byte("foo")
+	blobPath := writeFile(t, td, string(blob), "foo.txt")
+
+	rootOpts := &options.RootOptions{}
+	keyOpts := options.KeyOpts{KeyRef: keyRef}
+
+	// Test happy path
+	_, err := SignBlobCmd(rootOpts, keyOpts, blobPath, false, "", "", false)
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
+}
+
+func writeFile(t *testing.T, td string, blob string, name string) string {
+	// Write blob to disk
+	blobPath := filepath.Join(td, name)
+	if err := os.WriteFile(blobPath, []byte(blob), 0644); err != nil {
+		t.Fatal(err)
+	}
+	return blobPath
+}
