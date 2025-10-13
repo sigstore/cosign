b529ddf789f79cace95db56691e52b455077dbd5
Colleen Murphy <cmurphy@users.noreply.github.com>
2025-10-13 13:35:20 -0700

    Deduplicate code in sign/attest* and verify* commands (#4449)
    * Deduplicate key/token handling in sign commands
    
    Move the nearly identical code for parsing key options and creating a
    key pair and token out of attest, attest-blob, sign, and sign-blob, and
    into a common helper package. Move functions that had been shared out of
    sign.go into the helper package too so that other commands do not have
    to import the sign command package.
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Deduplicate signer-verifier creation
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Deduplicate timestamp retrieval
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Deduplicate rekor upload
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Deduplicate bundle compilation
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Move OCI parsing function to signcommon
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Make flag compatibility checking consistent
    
    Move flag checks when --new-bundle-format is used to a common helper
    module and have all four verify commands use it. Remove redundant flag
    checker code.
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Remove duplicate certs setting
    
    RootCerts and IntermediateCerts are already set on CheckOpts during
    loadCertsKeylessVerification.
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Move loading key to common
    
    Move the setting of SigVerifier based on the key ref, key slot, or cert
    and cert chain, to the common file.
    
    For verifying blobs and blob attestations with a certificate instead of
    a key, we return the cert which is used directly in the options list for
    verification. For images, the cert and cert chain must be validated and
    then unpacked into the SigVerifier, where the cosign Verify* functions
    check its validity by extracting it from the verifier.
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Deduplicate TUF v1 fetch and rekor client setup
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Deduplicate trusted material setting
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Move common functions to common.go
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    ---------
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>

---
+++ cmd/cosign/cli/verify/common.go
@@ -0,0 +1,465 @@
+// Copyright 2025 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package verify
+
+import (
+	"bytes"
+	"context"
+	"crypto"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+
+	"github.com/sigstore/cosign/v3/cmd/cosign/cli/fulcio"
+	"github.com/sigstore/cosign/v3/cmd/cosign/cli/options"
+	"github.com/sigstore/cosign/v3/cmd/cosign/cli/rekor"
+	"github.com/sigstore/cosign/v3/internal/ui"
+	"github.com/sigstore/cosign/v3/pkg/blob"
+	"github.com/sigstore/cosign/v3/pkg/cosign"
+	"github.com/sigstore/cosign/v3/pkg/cosign/env"
+	"github.com/sigstore/cosign/v3/pkg/cosign/pivkey"
+	"github.com/sigstore/cosign/v3/pkg/cosign/pkcs11key"
+	"github.com/sigstore/cosign/v3/pkg/oci"
+	csignature "github.com/sigstore/cosign/v3/pkg/signature"
+	"github.com/sigstore/sigstore-go/pkg/root"
+	"github.com/sigstore/sigstore/pkg/cryptoutils"
+	"github.com/sigstore/sigstore/pkg/signature"
+	"github.com/sigstore/sigstore/pkg/signature/payload"
+)
+
+// CheckSigstoreBundleUnsupportedOptions checks for incompatible settings on any Verify* command struct when NewBundleFormat is used.
+func CheckSigstoreBundleUnsupportedOptions(cmd any, co *cosign.CheckOpts) error {
+	if !co.NewBundleFormat {
+		return nil
+	}
+	fieldToErr := map[string]string{
+		"CertRef":              "certificate must be in bundle and may not be provided using --certificate",
+		"CertChain":            "certificate chain must be in bundle and may not be provided using --certificate-chain",
+		"CARoots":              "CA roots/intermediates must be provided using --trusted-root",
+		"CAIntermedias":        "CA roots/intermediates must be provided using --trusted-root",
+		"TSACertChainPath":     "TSA certificate chain path may only be provided using --trusted-root",
+		"RFC3161TimestampPath": "RFC3161 timestamp may not be provided using --rfc3161-timestamp",
+		"SigRef":               "signature may not be provided using --signature",
+		"SCTRef":               "SCT may not be provided using --sct",
+	}
+	v := reflect.ValueOf(cmd)
+	for f, e := range fieldToErr {
+		if field := v.FieldByName(f); field.IsValid() && field.String() != "" {
+			return fmt.Errorf("unsupported: %s when using --new-bundle-format", e)
+		}
+	}
+	if co.TrustedMaterial == nil {
+		return fmt.Errorf("trusted root is required when using new bundle format")
+	}
+	return nil
+}
+
+// LoadVerifierFromKeyOrCert returns either a signature.Verifier or a certificate from the provided flags to use for verifying an artifact.
+// In the case of certain types of keys, it returns a close function that must be called by the calling method.
+func LoadVerifierFromKeyOrCert(ctx context.Context, keyRef, slot, certRef, certChain string, hashAlgorithm crypto.Hash, sk, withGetCert bool, co *cosign.CheckOpts) (signature.Verifier, *x509.Certificate, func(), error) {
+	var sigVerifier signature.Verifier
+	var err error
+	switch {
+	case keyRef != "":
+		sigVerifier, err = csignature.PublicKeyFromKeyRefWithHashAlgo(ctx, keyRef, hashAlgorithm)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("loading public key: %w", err)
+		}
+		pkcs11Key, ok := sigVerifier.(*pkcs11key.Key)
+		closeSV := func() {}
+		if ok {
+			closeSV = pkcs11Key.Close
+		}
+		return sigVerifier, nil, closeSV, nil
+	case sk:
+		sk, err := pivkey.GetKeyWithSlot(slot)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("opening piv token: %w", err)
+		}
+		sigVerifier, err = sk.Verifier()
+		if err != nil {
+			sk.Close()
+			return nil, nil, nil, fmt.Errorf("initializing piv token verifier: %w", err)
+		}
+		return sigVerifier, nil, sk.Close, nil
+	case certRef != "":
+		cert, err := loadCertFromFileOrURL(certRef)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("loading cert: %w", err)
+		}
+		if withGetCert {
+			return nil, cert, func() {}, nil
+		}
+		if certChain == "" {
+			sigVerifier, err = cosign.ValidateAndUnpackCert(cert, co)
+			if err != nil {
+				return nil, nil, nil, fmt.Errorf("validating cert: %w", err)
+			}
+			return sigVerifier, nil, func() {}, nil
+		}
+		chain, err := loadCertChainFromFileOrURL(certChain)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("loading cert chain: %w", err)
+		}
+		sigVerifier, err = cosign.ValidateAndUnpackCertWithChain(cert, chain, co)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("validating cert with chain: %w", err)
+		}
+		return sigVerifier, nil, func() {}, nil
+	}
+	return nil, nil, func() {}, nil
+}
+
+// SetLegacyClientsAndKeys sets up TSA and rekor clients and keys for TSA, rekor, and CT log.
+// It may perform an online fetch of keys, so using trusted root instead of these TUF v1 methos is recommended.
+// It takes a CheckOpts as input and modifies it.
+func SetLegacyClientsAndKeys(ctx context.Context, ignoreTlog, shouldVerifySCT, keylessVerification bool, rekorURL, tsaCertChain, certChain, caRoots, caIntermediates string, co *cosign.CheckOpts) error {
+	var err error
+	if !ignoreTlog && !co.NewBundleFormat && rekorURL != "" {
+		co.RekorClient, err = rekor.NewClient(rekorURL)
+		if err != nil {
+			return fmt.Errorf("creating rekor client: %w", err)
+		}
+	}
+	// If trusted material is set, we don't need to fetch disparate keys.
+	if co.TrustedMaterial != nil {
+		return nil
+	}
+	if co.UseSignedTimestamps {
+		tsaCertificates, err := cosign.GetTSACerts(ctx, tsaCertChain, cosign.GetTufTargets)
+		if err != nil {
+			return fmt.Errorf("loading TSA certificates: %w", err)
+		}
+		co.TSACertificate = tsaCertificates.LeafCert
+		co.TSARootCertificates = tsaCertificates.RootCert
+		co.TSAIntermediateCertificates = tsaCertificates.IntermediateCerts
+	}
+	if !ignoreTlog {
+		co.RekorPubKeys, err = cosign.GetRekorPubs(ctx)
+		if err != nil {
+			return fmt.Errorf("getting rekor public keys: %w", err)
+		}
+	}
+	if shouldVerifySCT {
+		co.CTLogPubKeys, err = cosign.GetCTLogPubs(ctx)
+		if err != nil {
+			return fmt.Errorf("getting ctlog public keys: %w", err)
+		}
+	}
+	if keylessVerification {
+		if err := loadCertsKeylessVerification(certChain, caRoots, caIntermediates, co); err != nil {
+			return fmt.Errorf("loading certs for keyless verification: %w", err)
+		}
+	}
+	return nil
+}
+
+// SetTrustedMaterial sets TrustedMaterial on CheckOpts, either from the provided trusted root path or from TUF.
+// It does not set TrustedMaterial if the user provided trusted material via other flags or environment variables.
+func SetTrustedMaterial(ctx context.Context, trustedRootPath, certChain, caRoots, caIntermediates, tsaCertChainPath string, co *cosign.CheckOpts) error {
+	var err error
+	if trustedRootPath != "" {
+		co.TrustedMaterial, err = root.NewTrustedRootFromPath(trustedRootPath)
+		if err != nil {
+			return fmt.Errorf("loading trusted root: %w", err)
+		}
+		return nil
+	}
+	if options.NOf(certChain, caRoots, caIntermediates, tsaCertChainPath) == 0 &&
+		env.Getenv(env.VariableSigstoreCTLogPublicKeyFile) == "" &&
+		env.Getenv(env.VariableSigstoreRootFile) == "" &&
+		env.Getenv(env.VariableSigstoreRekorPublicKey) == "" &&
+		env.Getenv(env.VariableSigstoreTSACertificateFile) == "" {
+		co.TrustedMaterial, err = cosign.TrustedRoot()
+		if err != nil {
+			ui.Warnf(ctx, "Could not fetch trusted_root.json from the TUF repository. Continuing with individual targets. Error from TUF: %v", err)
+		}
+	}
+	return nil
+}
+
+// PrintVerificationHeader prints boilerplate information after successful verification.
+func PrintVerificationHeader(ctx context.Context, imgRef string, co *cosign.CheckOpts, bundleVerified, fulcioVerified bool) {
+	ui.Infof(ctx, "\nVerification for %s --", imgRef)
+	ui.Infof(ctx, "The following checks were performed on each of these signatures:")
+	if co.ClaimVerifier != nil {
+		if co.Annotations != nil {
+			ui.Infof(ctx, "  - The specified annotations were verified.")
+		}
+		ui.Infof(ctx, "  - The cosign claims were validated")
+	}
+	if bundleVerified {
+		ui.Infof(ctx, "  - Existence of the claims in the transparency log was verified offline")
+	} else if co.RekorClient != nil {
+		ui.Infof(ctx, "  - The claims were present in the transparency log")
+		ui.Infof(ctx, "  - The signatures were integrated into the transparency log when the certificate was valid")
+	}
+	if co.SigVerifier != nil {
+		ui.Infof(ctx, "  - The signatures were verified against the specified public key")
+	}
+	if fulcioVerified {
+		ui.Infof(ctx, "  - The code-signing certificate was verified using trusted certificate authority certificates")
+	}
+}
+
+// PrintVerification logs details about the verification to stdout.
+func PrintVerification(ctx context.Context, verified []oci.Signature, output string) {
+	switch output {
+	case "text":
+		for _, sig := range verified {
+			if cert, err := sig.Cert(); err == nil && cert != nil {
+				ce := cosign.CertExtensions{Cert: cert}
+				sub := ""
+				if sans := cryptoutils.GetSubjectAlternateNames(cert); len(sans) > 0 {
+					sub = sans[0]
+				}
+				ui.Infof(ctx, "Certificate subject: %s", sub)
+				if issuerURL := ce.GetIssuer(); issuerURL != "" {
+					ui.Infof(ctx, "Certificate issuer URL: %s", issuerURL)
+				}
+
+				if githubWorkflowTrigger := ce.GetCertExtensionGithubWorkflowTrigger(); githubWorkflowTrigger != "" {
+					ui.Infof(ctx, "GitHub Workflow Trigger: %s", githubWorkflowTrigger)
+				}
+
+				if githubWorkflowSha := ce.GetExtensionGithubWorkflowSha(); githubWorkflowSha != "" {
+					ui.Infof(ctx, "GitHub Workflow SHA: %s", githubWorkflowSha)
+				}
+				if githubWorkflowName := ce.GetCertExtensionGithubWorkflowName(); githubWorkflowName != "" {
+					ui.Infof(ctx, "GitHub Workflow Name: %s", githubWorkflowName)
+				}
+
+				if githubWorkflowRepository := ce.GetCertExtensionGithubWorkflowRepository(); githubWorkflowRepository != "" {
+					ui.Infof(ctx, "GitHub Workflow Repository: %s", githubWorkflowRepository)
+				}
+
+				if githubWorkflowRef := ce.GetCertExtensionGithubWorkflowRef(); githubWorkflowRef != "" {
+					ui.Infof(ctx, "GitHub Workflow Ref: %s", githubWorkflowRef)
+				}
+			}
+
+			p, err := sig.Payload()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error fetching payload: %v", err)
+				return
+			}
+			fmt.Println(string(p))
+		}
+
+	default:
+		var outputKeys []payload.SimpleContainerImage
+		for _, sig := range verified {
+			p, err := sig.Payload()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error fetching payload: %v", err)
+				return
+			}
+
+			ss := payload.SimpleContainerImage{}
+			if err := json.Unmarshal(p, &ss); err != nil {
+				fmt.Println("error decoding the payload:", err.Error())
+				return
+			}
+
+			if cert, err := sig.Cert(); err == nil && cert != nil {
+				ce := cosign.CertExtensions{Cert: cert}
+				if ss.Optional == nil {
+					ss.Optional = make(map[string]interface{})
+				}
+				sub := ""
+				if sans := cryptoutils.GetSubjectAlternateNames(cert); len(sans) > 0 {
+					sub = sans[0]
+				}
+				ss.Optional["Subject"] = sub
+				if issuerURL := ce.GetIssuer(); issuerURL != "" {
+					ss.Optional["Issuer"] = issuerURL
+					ss.Optional[cosign.CertExtensionOIDCIssuer] = issuerURL
+				}
+				if githubWorkflowTrigger := ce.GetCertExtensionGithubWorkflowTrigger(); githubWorkflowTrigger != "" {
+					ss.Optional[cosign.CertExtensionMap[cosign.CertExtensionGithubWorkflowTrigger]] = githubWorkflowTrigger
+					ss.Optional[cosign.CertExtensionGithubWorkflowTrigger] = githubWorkflowTrigger
+				}
+
+				if githubWorkflowSha := ce.GetExtensionGithubWorkflowSha(); githubWorkflowSha != "" {
+					ss.Optional[cosign.CertExtensionMap[cosign.CertExtensionGithubWorkflowSha]] = githubWorkflowSha
+					ss.Optional[cosign.CertExtensionGithubWorkflowSha] = githubWorkflowSha
+				}
+				if githubWorkflowName := ce.GetCertExtensionGithubWorkflowName(); githubWorkflowName != "" {
+					ss.Optional[cosign.CertExtensionMap[cosign.CertExtensionGithubWorkflowName]] = githubWorkflowName
+					ss.Optional[cosign.CertExtensionGithubWorkflowName] = githubWorkflowName
+				}
+
+				if githubWorkflowRepository := ce.GetCertExtensionGithubWorkflowRepository(); githubWorkflowRepository != "" {
+					ss.Optional[cosign.CertExtensionMap[cosign.CertExtensionGithubWorkflowRepository]] = githubWorkflowRepository
+					ss.Optional[cosign.CertExtensionGithubWorkflowRepository] = githubWorkflowRepository
+				}
+
+				if githubWorkflowRef := ce.GetCertExtensionGithubWorkflowRef(); githubWorkflowRef != "" {
+					ss.Optional[cosign.CertExtensionMap[cosign.CertExtensionGithubWorkflowRef]] = githubWorkflowRef
+					ss.Optional[cosign.CertExtensionGithubWorkflowRef] = githubWorkflowRef
+				}
+			}
+			if bundle, err := sig.Bundle(); err == nil && bundle != nil {
+				if ss.Optional == nil {
+					ss.Optional = make(map[string]interface{})
+				}
+				ss.Optional["Bundle"] = bundle
+			}
+			if rfc3161Timestamp, err := sig.RFC3161Timestamp(); err == nil && rfc3161Timestamp != nil {
+				if ss.Optional == nil {
+					ss.Optional = make(map[string]interface{})
+				}
+				ss.Optional["RFC3161Timestamp"] = rfc3161Timestamp
+			}
+
+			outputKeys = append(outputKeys, ss)
+		}
+
+		b, err := json.Marshal(outputKeys)
+		if err != nil {
+			fmt.Println("error when generating the output:", err.Error())
+			return
+		}
+
+		fmt.Printf("\n%s\n", string(b))
+	}
+}
+
+func loadCertFromFileOrURL(path string) (*x509.Certificate, error) {
+	pems, err := blob.LoadFileOrURL(path)
+	if err != nil {
+		return nil, err
+	}
+	return loadCertFromPEM(pems)
+}
+
+func loadCertFromPEM(pems []byte) (*x509.Certificate, error) {
+	var out []byte
+	out, err := base64.StdEncoding.DecodeString(string(pems))
+	if err != nil {
+		// not a base64
+		out = pems
+	}
+
+	certs, err := cryptoutils.UnmarshalCertificatesFromPEM(out)
+	if err != nil {
+		return nil, err
+	}
+	if len(certs) == 0 {
+		return nil, errors.New("no certs found in pem file")
+	}
+	return certs[0], nil
+}
+
+func loadCertChainFromFileOrURL(path string) ([]*x509.Certificate, error) {
+	pems, err := blob.LoadFileOrURL(path)
+	if err != nil {
+		return nil, err
+	}
+	certs, err := cryptoutils.LoadCertificatesFromPEM(bytes.NewReader(pems))
+	if err != nil {
+		return nil, err
+	}
+	return certs, nil
+}
+
+func keylessVerification(keyRef string, sk bool) bool {
+	if keyRef != "" {
+		return false
+	}
+	if sk {
+		return false
+	}
+	return true
+}
+
+func shouldVerifySCT(ignoreSCT bool, keyRef string, sk bool) bool {
+	if keyRef != "" {
+		return false
+	}
+	if sk {
+		return false
+	}
+	if ignoreSCT {
+		return false
+	}
+	return true
+}
+
+// loadCertsKeylessVerification loads certificates provided as a certificate chain or CA roots + CA intermediate
+// certificate files. If both certChain and caRootsFile are empty strings, the Fulcio roots are loaded.
+//
+// The co *cosign.CheckOpts is both input and output parameter - it gets updated
+// with the root and intermediate certificates needed for verification.
+func loadCertsKeylessVerification(certChainFile string,
+	caRootsFile string,
+	caIntermediatesFile string,
+	co *cosign.CheckOpts) error {
+	var err error
+	switch {
+	case certChainFile != "":
+		chain, err := loadCertChainFromFileOrURL(certChainFile)
+		if err != nil {
+			return err
+		}
+		co.RootCerts = x509.NewCertPool()
+		co.RootCerts.AddCert(chain[len(chain)-1])
+		if len(chain) > 1 {
+			co.IntermediateCerts = x509.NewCertPool()
+			for _, cert := range chain[:len(chain)-1] {
+				co.IntermediateCerts.AddCert(cert)
+			}
+		}
+	case caRootsFile != "":
+		caRoots, err := loadCertChainFromFileOrURL(caRootsFile)
+		if err != nil {
+			return err
+		}
+		co.RootCerts = x509.NewCertPool()
+		if len(caRoots) > 0 {
+			for _, cert := range caRoots {
+				co.RootCerts.AddCert(cert)
+			}
+		}
+		if caIntermediatesFile != "" {
+			caIntermediates, err := loadCertChainFromFileOrURL(caIntermediatesFile)
+			if err != nil {
+				return err
+			}
+			if len(caIntermediates) > 0 {
+				co.IntermediateCerts = x509.NewCertPool()
+				for _, cert := range caIntermediates {
+					co.IntermediateCerts.AddCert(cert)
+				}
+			}
+		}
+	default:
+		// This performs an online fetch of the Fulcio roots from a TUF repository.
+		// This is needed for verifying keyless certificates (both online and offline).
+		co.RootCerts, err = fulcio.GetRoots()
+		if err != nil {
+			return fmt.Errorf("getting Fulcio roots: %w", err)
+		}
+		co.IntermediateCerts, err = fulcio.GetIntermediates()
+		if err != nil {
+			return fmt.Errorf("getting Fulcio intermediates: %w", err)
+		}
+	}
+
+	return nil
+}
--- cmd/cosign/cli/verify/verify_blob_attestation.go
+++ cmd/cosign/cli/verify/verify_blob_attestation.go
@@ -30,21 +30,16 @@ import (
 
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/sigstore/cosign/v3/cmd/cosign/cli/options"
-	"github.com/sigstore/cosign/v3/cmd/cosign/cli/rekor"
 	internal "github.com/sigstore/cosign/v3/internal/pkg/cosign"
 	payloadsize "github.com/sigstore/cosign/v3/internal/pkg/cosign/payload/size"
 	"github.com/sigstore/cosign/v3/internal/ui"
 	"github.com/sigstore/cosign/v3/pkg/blob"
 	"github.com/sigstore/cosign/v3/pkg/cosign"
 	"github.com/sigstore/cosign/v3/pkg/cosign/bundle"
-	"github.com/sigstore/cosign/v3/pkg/cosign/env"
-	"github.com/sigstore/cosign/v3/pkg/cosign/pivkey"
-	"github.com/sigstore/cosign/v3/pkg/cosign/pkcs11key"
 	"github.com/sigstore/cosign/v3/pkg/oci/static"
 	"github.com/sigstore/cosign/v3/pkg/policy"
 	sigs "github.com/sigstore/cosign/v3/pkg/signature"
 	sgbundle "github.com/sigstore/sigstore-go/pkg/bundle"
-	"github.com/sigstore/sigstore-go/pkg/root"
 	sgverify "github.com/sigstore/sigstore-go/pkg/verify"
 	"github.com/sigstore/sigstore/pkg/cryptoutils"
 )
@@ -128,37 +123,13 @@ func (c *VerifyBlobAttestationCommand) Exec(ctx context.Context, artifactPath st
 	}
 
 	// Keys are optional!
+	var closeSV func()
 	var cert *x509.Certificate
-	opts := make([]static.Option, 0)
-	switch {
-	case c.KeyRef != "":
-		co.SigVerifier, err = sigs.PublicKeyFromKeyRefWithHashAlgo(ctx, c.KeyRef, c.HashAlgorithm)
-		if err != nil {
-			return fmt.Errorf("loading public key: %w", err)
-		}
-		pkcs11Key, ok := co.SigVerifier.(*pkcs11key.Key)
-		if ok {
-			defer pkcs11Key.Close()
-		}
-	case c.Sk:
-		sk, err := pivkey.GetKeyWithSlot(c.Slot)
-		if err != nil {
-			return fmt.Errorf("opening piv token: %w", err)
-		}
-		defer sk.Close()
-		co.SigVerifier, err = sk.Verifier()
-		if err != nil {
-			return fmt.Errorf("loading public key from token: %w", err)
-		}
-	case c.CertRef != "":
-		cert, err = loadCertFromFileOrURL(c.CertRef)
-		if err != nil {
-			return err
-		}
-	case c.CARoots != "":
-		// CA roots + possible intermediates are already loaded into co.RootCerts with the call to
-		// loadCertsKeylessVerification above.
+	co.SigVerifier, cert, closeSV, err = LoadVerifierFromKeyOrCert(ctx, c.KeyRef, c.Slot, c.CertRef, "", c.HashAlgorithm, c.Sk, true, co)
+	if err != nil {
+		return fmt.Errorf("loading verifier from key opts: %w", err)
 	}
+	defer closeSV()
 
 	var h v1.Hash
 	var digest []byte
@@ -205,31 +176,16 @@ func (c *VerifyBlobAttestationCommand) Exec(ctx context.Context, artifactPath st
 		co.ClaimVerifier = cosign.IntotoSubjectClaimVerifier
 	}
 
-	if c.TrustedRootPath != "" {
-		co.TrustedMaterial, err = root.NewTrustedRootFromPath(c.TrustedRootPath)
-		if err != nil {
-			return fmt.Errorf("loading trusted root: %w", err)
-		}
-	} else if options.NOf(c.CertChain, c.CARoots, c.CAIntermediates, c.TSACertChainPath) == 0 &&
-		env.Getenv(env.VariableSigstoreCTLogPublicKeyFile) == "" &&
-		env.Getenv(env.VariableSigstoreRootFile) == "" &&
-		env.Getenv(env.VariableSigstoreRekorPublicKey) == "" &&
-		env.Getenv(env.VariableSigstoreTSACertificateFile) == "" {
-		co.TrustedMaterial, err = cosign.TrustedRoot()
-		if err != nil {
-			ui.Warnf(ctx, "Could not fetch trusted_root.json from the TUF repository. Continuing with individual targets. Error from TUF: %v", err)
-		}
+	err = SetTrustedMaterial(ctx, c.TrustedRootPath, c.CertChain, c.CARoots, c.CAIntermediates, c.TSACertChainPath, co)
+	if err != nil {
+		return fmt.Errorf("setting trusted material: %w", err)
+	}
+
+	if err = CheckSigstoreBundleUnsupportedOptions(*c, co); err != nil {
+		return err
 	}
 
 	if co.NewBundleFormat {
-		if options.NOf(c.RFC3161TimestampPath, c.TSACertChainPath, c.CertChain, c.CARoots, c.CAIntermediates, c.CertRef, c.SCTRef) > 0 {
-			return fmt.Errorf("when using --new-bundle-format, please supply signed content with --bundle and verification content with --trusted-root")
-		}
-
-		if co.TrustedMaterial == nil {
-			return fmt.Errorf("trusted root is required when using new bundle format")
-		}
-
 		bundle, err := sgbundle.LoadJSONFromPath(c.BundlePath)
 		if err != nil {
 			return err
@@ -259,45 +215,10 @@ func (c *VerifyBlobAttestationCommand) Exec(ctx context.Context, artifactPath st
 	} else if c.RFC3161TimestampPath == "" && co.UseSignedTimestamps {
 		return fmt.Errorf("when specifying --use-signed-timestamps or --timestamp-certificate-chain, you must also specify --rfc3161-timestamp-path")
 	}
-	if co.UseSignedTimestamps && co.TrustedMaterial == nil {
-		tsaCertificates, err := cosign.GetTSACerts(ctx, c.TSACertChainPath, cosign.GetTufTargets)
-		if err != nil {
-			return fmt.Errorf("unable to load TSA certificates: %w", err)
-		}
-		co.TSACertificate = tsaCertificates.LeafCert
-		co.TSARootCertificates = tsaCertificates.RootCert
-		co.TSAIntermediateCertificates = tsaCertificates.IntermediateCerts
-	}
 
-	if !c.IgnoreTlog {
-		if c.RekorURL != "" {
-			rekorClient, err := rekor.NewClient(c.RekorURL)
-			if err != nil {
-				return fmt.Errorf("creating Rekor client: %w", err)
-			}
-			co.RekorClient = rekorClient
-		}
-		if co.TrustedMaterial == nil {
-			// This performs an online fetch of the Rekor public keys, but this is needed
-			// for verifying tlog entries (both online and offline).
-			co.RekorPubKeys, err = cosign.GetRekorPubs(ctx)
-			if err != nil {
-				return fmt.Errorf("getting Rekor public keys: %w", err)
-			}
-		}
-	}
-	if co.TrustedMaterial == nil && keylessVerification(c.KeyRef, c.Sk) {
-		if err := loadCertsKeylessVerification(c.CertChain, c.CARoots, c.CAIntermediates, co); err != nil {
-			return err
-		}
-	}
-
-	// Ignore Signed Certificate Timestamp if the flag is set or a key is provided
-	if co.TrustedMaterial == nil && shouldVerifySCT(c.IgnoreSCT, c.KeyRef, c.Sk) {
-		co.CTLogPubKeys, err = cosign.GetCTLogPubs(ctx)
-		if err != nil {
-			return fmt.Errorf("getting ctlog public keys: %w", err)
-		}
+	err = SetLegacyClientsAndKeys(ctx, c.IgnoreTlog, shouldVerifySCT(c.IgnoreSCT, c.KeyRef, c.Sk), keylessVerification(c.KeyRef, c.Sk), c.RekorURL, c.TSACertChainPath, c.CertChain, c.CARoots, c.CAIntermediates, co)
+	if err != nil {
+		return fmt.Errorf("setting up clients and keys: %w", err)
 	}
 
 	var encodedSig []byte
@@ -308,6 +229,7 @@ func (c *VerifyBlobAttestationCommand) Exec(ctx context.Context, artifactPath st
 		}
 	}
 
+	opts := make([]static.Option, 0)
 	if c.BundlePath != "" {
 		b, err := cosign.FetchLocalSignedPayloadFromPath(c.BundlePath)
 		if err != nil {
