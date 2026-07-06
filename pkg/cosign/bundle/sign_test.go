784e12b0d807f38538576f03ed5527687d39675c
Kenneth Yang <kenneth.yang@coinbase.com>
2026-07-06 12:12:03 -0700

    Update Copyright Date
    Signed-off-by: Kenneth Yang <kenneth.yang@coinbase.com>

--- pkg/cosign/bundle/sign_test.go
+++ pkg/cosign/bundle/sign_test.go
@@ -1,4 +1,4 @@
-// Copyright 2025 The Sigstore Authors.
+// Copyright 2026 The Sigstore Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
